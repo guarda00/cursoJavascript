1d5ecd9e84dd8f393e6e198ddc6cdd68ebca30bb
Luis Alvarez Almeida <lalvarez@prueba.com>
2022-05-18 21:18:50 -0500

    Formulario html y funcion calcular perimetro

---
+++ figuras.js
@@ -0,0 +1,10 @@
+function areaCuadrado(lado){
+    return lado * 4;
+}
+function calcularPerimetroCuadrado(){
+    const input = document.getElementById("InputCuadrado").value;
+    //const value = input.value;
+    const area = areaCuadrado(input);
+    alert(area);
+
+}
